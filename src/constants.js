04c4fd67dc790577d1a8c44220d0b7d736f8ea7a
Aight Bit <45626542+aightbit0@users.noreply.github.com>
2022-04-16 18:36:34 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,2 @@
-export const BACKENDURL = "http://85.214.39.186:8080";
-export const RELOADURL = "http://playlist-together.we-make-your.software";
-//export const BACKENDURL = "http://192.168.0.73:8080";
-//export const RELOADURL = "http://localhost:3000";
+export const BACKENDURL = "http://exampleUrl.com:8080";
+export const RELOADURL = "http://exampleUrl.com";
